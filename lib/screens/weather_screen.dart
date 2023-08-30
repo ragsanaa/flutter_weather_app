2ff90fedcaec972445e46d2bee12e4c9e0d166a4
Raqsana <raqsana.alizade@wetravel.com>
2023-08-30 04:12:40 +0400

    add languages

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,9 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_weather/constants/languages.dart';
+import 'package:flutter_weather/constants/terms_translations.dart';
 import 'package:flutter_weather/models/weather_model.dart';
 import 'package:flutter_weather/services/weather_service.dart';
 import 'package:geolocator/geolocator.dart';
+
+import 'package:material_symbols_icons/symbols.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
@@ -15,6 +19,13 @@ class WeatherScreen extends StatefulWidget {
 class _WeatherScreenState extends State<WeatherScreen> {
   Position? _currentPosition;
   String cityName = '';
+  String selectedValue = 'en';
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentPosition();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +69,8 @@ class _WeatherScreenState extends State<WeatherScreen> {
               future: WeatherService().getWeatherDetails(
                   location: cityName,
                   lat: _currentPosition?.latitude ?? 0,
-                  lon: _currentPosition?.longitude ?? 0),
+                  lon: _currentPosition?.longitude ?? 0,
+                  lang: selectedValue),
               builder: (BuildContext context, AsyncSnapshot snapshot) {
                 if (snapshot.hasError) {
                   return Center(
@@ -89,72 +101,124 @@ class _WeatherScreenState extends State<WeatherScreen> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Text(
-                                        weather.name!,
-                                        style: TextStyle(
-                                          fontSize: 32,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                      IconButton(
-                                        onPressed: () {
-                                          showDialog(
-                                            context: context,
-                                            builder: (BuildContext context) {
-                                              return alert;
-                                            },
-                                          );
-                                        },
-                                        icon: Icon(
-                                          Icons.search,
-                                          color: Colors.white,
-                                          size: 30,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  Stack(children: [
-                                    Text(
-                                      '${weather.main!.temp!.toStringAsFixed(0)} ',
-                                      style: TextStyle(
-                                        fontSize: 100,
-                                        color: Colors.white,
-                                      ),
-                                      textScaleFactor: 2,
-                                    ),
-                                    Positioned(
-                                      right: 0,
-                                      child: Text(
-                                        '°',
-                                        style: TextStyle(
-                                          fontSize: 100,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ),
-                                  ]),
-                                ],
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Container(
+                              margin: EdgeInsets.only(bottom: 10),
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 10,
                               ),
-                              RotatedBox(
-                                quarterTurns: -1,
-                                child: Text(
-                                  weather.weather![0].description!,
-                                  style: TextStyle(
-                                    fontSize: 26,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                              decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(0.3),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: DropdownButton(
+                                icon: Icon(
+                                  Icons.arrow_drop_down,
+                                  color: Colors.white,
                                 ),
-                              )
-                            ],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                                dropdownColor: Colors.black.withOpacity(0.6),
+                                elevation: 0,
+                                value: selectedValue,
+                                onChanged: (value) {
+                                  setState(() {
+                                    selectedValue = value.toString();
+                                  });
+                                  build(context);
+                                },
+                                underline: SizedBox(),
+                                items: languages.entries
+                                    .map((e) => DropdownMenuItem(
+                                          value: e.key,
+                                          child: Text(e.value),
+                                        ))
+                                    .toList(),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              Expanded(
+                                                child: Text(
+                                                  '${weather.name}, ${weather.sys!.country}',
+                                                  style: TextStyle(
+                                                    fontSize: 32,
+                                                    color: Colors.white,
+                                                  ),
+                                                ),
+                                              ),
+                                              IconButton(
+                                                onPressed: () {
+                                                  showDialog(
+                                                    context: context,
+                                                    builder:
+                                                        (BuildContext context) {
+                                                      return alert;
+                                                    },
+                                                  );
+                                                },
+                                                icon: Icon(
+                                                  Icons.search,
+                                                  color: Colors.white,
+                                                  size: 30,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          Stack(children: [
+                                            Text(
+                                              '${weather.main!.temp!.toStringAsFixed(0)} ',
+                                              style: TextStyle(
+                                                fontSize: 100,
+                                                color: Colors.white,
+                                              ),
+                                              textScaleFactor: 2,
+                                            ),
+                                            Positioned(
+                                              right: 0,
+                                              child: Text(
+                                                '°',
+                                                style: TextStyle(
+                                                  fontSize: 100,
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                            ),
+                                          ]),
+                                        ],
+                                      ),
+                                    ),
+                                    RotatedBox(
+                                      quarterTurns: -1,
+                                      child: Text(
+                                        weather.weather![0].description!,
+                                        style: TextStyle(
+                                          fontSize: 26,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                           Container(
                             padding: EdgeInsets.symmetric(
@@ -174,6 +238,9 @@ class _WeatherScreenState extends State<WeatherScreen> {
                               children: [
                                 Column(
                                   children: [
+                                    Icon(Symbols.humidity_percentage,
+                                        color: Colors.white, size: 30),
+                                    const SizedBox(height: 10),
                                     Text(
                                       '${weather.main!.humidity!.toString()}%',
                                       style: TextStyle(
@@ -182,8 +249,11 @@ class _WeatherScreenState extends State<WeatherScreen> {
                                         color: Colors.white,
                                       ),
                                     ),
+                                    const SizedBox(height: 5),
                                     Text(
-                                      'Humidity',
+                                      termTranslations['humidity']![
+                                              selectedValue] ??
+                                          'Humidity',
                                       style: TextStyle(
                                         fontSize: 16,
                                         color: Colors.white,
@@ -193,6 +263,9 @@ class _WeatherScreenState extends State<WeatherScreen> {
                                 ),
                                 Column(
                                   children: [
+                                    Icon(Symbols.visibility,
+                                        color: Colors.white, size: 30),
+                                    const SizedBox(height: 10),
                                     Text(
                                       '${(weather.visibility! / 1000).toStringAsFixed(1)} km',
                                       style: TextStyle(
@@ -201,8 +274,11 @@ class _WeatherScreenState extends State<WeatherScreen> {
                                         color: Colors.white,
                                       ),
                                     ),
+                                    const SizedBox(height: 5),
                                     Text(
-                                      'Visibility',
+                                      termTranslations['visibility']![
+                                              selectedValue] ??
+                                          'Visibility',
                                       style: TextStyle(
                                         fontSize: 16,
                                         color: Colors.white,
@@ -212,6 +288,9 @@ class _WeatherScreenState extends State<WeatherScreen> {
                                 ),
                                 Column(
                                   children: [
+                                    Icon(Symbols.air,
+                                        color: Colors.white, size: 30),
+                                    const SizedBox(height: 10),
                                     Text(
                                       '${(weather.wind!.speed! * 3.6).toStringAsFixed(1)} km/h',
                                       style: TextStyle(
@@ -220,8 +299,11 @@ class _WeatherScreenState extends State<WeatherScreen> {
                                         fontWeight: FontWeight.bold,
                                       ),
                                     ),
+                                    const SizedBox(height: 5),
                                     Text(
-                                      'Wind',
+                                      termTranslations['wind']![
+                                              selectedValue] ??
+                                          'Wind',
                                       style: TextStyle(
                                         fontSize: 16,
                                         color: Colors.white,
